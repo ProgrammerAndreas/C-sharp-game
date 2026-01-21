218090bb1b3f49bc7d9d54739c64bb7d16e03d16
Andreas Fritzbøger <andreasfritzboger@live.dk>
2026-01-21 13:19:08 +0100

    Adding Pidgeon v1

--- Game/Pidgeon.cs
+++ Game/Pidgeon.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace PidgeonCarrier.Game
 {
-    internal class Pidgeon
+    public class Pidgeon
     {
+        public PointF Position { get; private set; }
+        public float Velocity { get; private set; }
+
+        private readonly float gravity = 0.5F;
+        private readonly float flapStrength = -8f;
+
+        private readonly int width = 30;
+        private readonly int height = 30;
+
+        public Pidgeon(float startX, float startY)
+        {
+            Position = new PointF(startX, startY);
+            Velocity = 0;
+        }
+
+        public void Update()
+        {
+            Velocity += gravity;
+            Position = new PointF(Position.X, Position.Y + Velocity);
+        }
+
+        public void Flap()
+        {
+            Velocity = flapStrength;
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.FillEllipse(Brushes.Yellow, Position.X, Position.Y, width, height);
+        }
+
+        public bool IsOutOfBounds(int formHeight)
+        {
+            return Position.Y < 0 || Position.Y + height > formHeight;
+        }
+
+        public RectangleF GetBounds()
+        {
+            return new RectangleF(Position.X, Position.Y, width, height);
+        }
     }
 }
--- UI/MainForm.cs
+++ UI/MainForm.cs
@@ -1,11 +1,14 @@
 namespace PidgeonCarrier
 {
+    using PidgeonCarrier.Game;
     using System.Windows.Forms;
 
     public partial class MainForm : Form
     {
         private readonly Timer _gameTimer = new();
         private const int TargetFps = 60;
+
+        private Pidgeon _pidgeon;
 
         public MainForm()
         {
@@ -14,9 +17,27 @@ namespace PidgeonCarrier
             DoubleBuffered = true;
             KeyPreview = true;
 
+            _pidgeon = new Pidgeon(100, this.ClientSize.Height / 2);
+
             _gameTimer.Interval = 1000 / TargetFps;
             _gameTimer.Tick += GameLoop;
             _gameTimer.Start();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            DrawGame(e.Graphics);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (e.KeyCode == Keys.Space)
+            {
+                _pidgeon.Flap();
+            }
         }
 
         private void GameLoop(object? sender, EventArgs e)
@@ -27,18 +48,22 @@ namespace PidgeonCarrier
 
         private void UpdateGame()
         {
-            // Game logic will go here
-        }
+            _pidgeon.Update();
 
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
-            DrawGame(e.Graphics);
+            if (_pidgeon.IsOutOfBounds(this.ClientSize.Height))
+            {
+                _gameTimer.Stop();
+                MessageBox.Show("Game Over!");
+                _pidgeon = new Pidgeon(100, this.ClientSize.Height / 2);
+                _gameTimer.Start();
+            }
         }
 
         private void DrawGame(Graphics graphics)
         {
             graphics.Clear(Color.SkyBlue);
+
+            _pidgeon.Draw(graphics);
         }
     }
 }
